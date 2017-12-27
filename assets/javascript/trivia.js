a63d9d760b752df2cf85219dd4dbbaba3b1e9305
mqamar1 <sufimehvish@yahoo.com>
2017-12-26 23:48:07 -0500

    update

--- assets/javascript/trivia.js
+++ assets/javascript/trivia.js
@@ -56,24 +56,27 @@ var gameQuestions = [{
         incorrect: 0,
         unanswered: 0,
 
+
         beginTime: function() {
           game.counter--;
           $("#counter").html(game.counter);
           if (game.counter === 0) {
             console.log("Time Up!")
-            game.timeUp();
+            game.timeup();
           }
         },
 
         beginQuestion: function() {
           timer = setInterval(game.beginTime, 1000);
-          $("#twowrapper").html("<h2> TIME REMAINING <span id='counter '>30</span> Seconds</h2>")
+          $("#twowrapper").append("<h2> Time Remaining: </h2>")
           $("#twowrapper").append("<h2>" + gameQuestions[game.questionNumber].question + "</h2>");
 
 
           for (var i = 0; i < gameQuestions[game.questionNumber].answers.length; i++) {
-            $("#twowrapper").append('<button class="answerButton"id ="button-'+i+'" data-name"'+gameQuestions[game.questionNumber].answers[i]
-            +'">'+gameQuestions[game.questionNumber].answers[i]+ "</button>")
+            $("#twowrapper").append("<button class='answerButton' id ='button-' data-name ='" + gameQuestions[game.questionNumber].answers[i]
+            +"'>"+gameQuestions[game.questionNumber].answers[i]+ "</button>");
+
+
 
           }
 
@@ -93,7 +96,7 @@ var gameQuestions = [{
         $("#twowrapper").html("<h2>Out of time!</h2>")
         $("#twowrapper").append("<h3>The correct Answer was: "+gameQuestions[game.questionNumber].correctAnswer);
         $("#twowrapper").append("<img src='" +gameQuestions[game.questionNumber].image +"'/>");
-        if(game.questionNumber==gameQuestions.length-1){
+        if(game.questionNumber===gameQuestions.length-1){
           setTimeout(game.results, 3*1000);
         }
         else{
@@ -104,7 +107,8 @@ var gameQuestions = [{
 
       results: function(){
         clearInterval(timer);
-        $("#twowrapper").html("<h2>All Done!</h2>");
+        $("#twowrapper").html("<h2>Your Results!</h2>");
+        $("#counter").html(game.counter);
         $("#twowrapper").append("<h3> Correct "+game.correct+"</h3>");
         $("#twowrapper").append("<h3> Incorrect "+game.incorrect+"</h3>");
         $("#twowrapper").append("<h3> Unanswered "+game.unanswered+"</h3>")
@@ -115,7 +119,7 @@ var gameQuestions = [{
 
       clicked: function(e){
         clearInterval(timer);
-        if ($(e.target).data("name")===gameQuestions[game.questionNumber].correctAnswer)
+        if ($(e.target).attr("data-name")===gameQuestions[game.questionNumber].correctAnswer)
         {
           game.answerCorrectly();
         }
